9a6cbc147fad7f838bd4b0071d11bc98c5bbd630
BalazsBanfi <banfibalazs81@gmail.com>
2023-11-08 21:54:02 +0100

    Add ship to row 33

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -11,69 +11,69 @@ const random = (x) => Math.floor(math.random() * x);
 
 // Factory function for create board
 export const gameboard = () => {
-    const board = [];
+    const board = {};
 
     // Creating a two-dimensional array with an object in it and fill with value: null
+    board.cells = [];
     for (let i = 0; i < 100; i++) {
-        board.push('null');
+        board.cells.push('null');
     }
-
-
-    // Check if the cell are free to place the ship
-    const goodPlace = (shipType, length, direction, startCoord) => {
-        const coordinates = [];
-
-        while (coordinates.length < 1) {
-
-            // Check the free cell in a row
-            if (direction === 0) {
-                if (startCoord % 10 + length > 9) {
-                    return false;
-                }
-                if (board.slice(startCoord, startCoord + length).every((x) => x === 'null')) {
-                    for (let i = 0; i < length; i++) {
-                        board[startCoord + i] = shipType;
-                        coordinates.push(startCoord + i);
-                    }
-                } else {
-                    return false;
-                }
-            }
-
-            // Check the free cell in a row
-
-            // Check the free cell in a row
-            if (direction === 0) {
-                if (Math.floor(startCoord / 10) + length > 9) {
-                    return false;
-                }
-                let tempArr = [];
-                for (let i = 0; i < length; i++) {
-                    tempArr.push(board[Math.floor(startCoord / 10) + i * 10]);
-                }
-                if (tempArr.every((x) => x === 'null')) {
-                    for (let i = 0; i < length; i++) {
-                        board[startCoord + (i * 10)] = shipType;
-                        coordinates.push(startCoord + (i * 10));
-                    }
-                } else {
-                    return false;
-                }
-            }
-
+    board.goodPlace = (startCoord, shipType, length) => {
+        for (let i = startCoord; i < startCoord + length; i++) {
+            board.cells[i] = shipType;
         }
-        return coordinates;
-    }
-
-    // Try to place the ships on random cells
-    const placeShip = () => {
-
-        Object.keys(ships).forEach((ship => {
-            let arr = goodPlace(ship, ships[ship]);
-            arr.forEach((coord) => {
-                board[coord[0]][coord[1]] = ship;
-            })
-        }));
     }
     return board;
+
+};
+/*
+
+// Check if the cell are free to place the ship
+const goodPlace = (shipType, length, direction, startCoord) => {
+    const coordinates = [];
+
+    while (coordinates.length < 1) {
+
+        // Check the free cell in a row
+        if (direction === 0) {
+            if (startCoord % 10 + length > 9) {
+                return false;
+            }
+            if (board.slice(startCoord, startCoord + length).every((x) => x === 'null')) {
+                for (let i = 0; i < length; i++) {
+                    board[startCoord + i] = shipType;
+                    coordinates.push(startCoord + i);
+                }
+            } else {
+                return false;
+            }
+        }
+
+        // Check the free cell in a row
+
+        // Check the free cell in a row
+        if (direction === 1) {
+            if (Math.floor(startCoord / 10) + length > 9) {
+                return false;
+            }
+            let tempArr = [];
+            for (let i = 0; i < length; i++) {
+                tempArr.push(board[Math.floor(startCoord / 10) + i * 10]);
+            }
+            if (tempArr.every((x) => x === 'null')) {
+                for (let i = 0; i < length; i++) {
+                    board[startCoord + (i * 10)] = shipType;
+                    coordinates.push(startCoord + (i * 10));
+                }
+            } else {
+                return false;
+            }
+        }
+
+    }
+    return coordinates;
 }
+
+return board;
+
+*/
--- src/factories/tests/gameboard.test.js
+++ src/factories/tests/gameboard.test.js
@@ -2,22 +2,37 @@ import { gameboard } from '../gameboard'
 
 test("Board is array", () => {
     let board1 = gameboard();
-    expect(Array.isArray(board1)).toBe(true);
+    expect(Array.isArray(board1.cells)).toBe(true);
 });
 
 test("Board has 100 cells", () => {
     let board1 = gameboard();
-    expect(board1.length).toBe(100);
+    expect(board1.cells.length).toBe(100);
 });
 
 test("One element of a board is null", () => {
     let board1 = gameboard();
-    expect(board1[47]).toBe('null');
+    expect(board1.cells[47]).toBe('null');
 });
 
 
-test("Place ship on random cell", () => {
+test("Place ship on random cell 33", () => {
     let board1 = gameboard();
-    board1.goodPlace('Carrier', 5, 0, 33);
-    expect(board1[33]).toBe('Carrier');
+    board1.goodPlace(33, 'Carrier', 5);
+    expect(board1.cells[33]).toBe('Carrier');
 });
+
+
+test("Place ship on cell 33, cell 36:", () => {
+    let board1 = gameboard();
+    board1.goodPlace(33, 'Carrier', 5);
+    expect(board1.cells[36]).toBe('Carrier');
+});
+
+
+test("Place ship on cell 33, cell 39:", () => {
+    let board1 = gameboard();
+    board1.goodPlace(33, 'Carrier', 5);
+    expect(board1.cells[39]).toBe('null');
+});
+
