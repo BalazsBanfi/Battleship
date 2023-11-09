c2a721b965ae51e22facb4c25862d897bdcf75ed
BalazsBanfi <banfibalazs81@gmail.com>
2023-11-09 14:45:17 +0100

    Prepare player board

--- src/render.js
+++ src/render.js
@@ -5,24 +5,42 @@ export const renderPage = () => {
     // Prepare computer board, place the boats randomly
     const boardComp = gameboard();
     boardComp.setComputerBoard();
+    const playerDiv = document.getElementById('boardPlayer')
+
     const compDiv = document.getElementById('boardComputer')
 
-    
+    // Render the board of the player
+    for (let i = 0; i < 100; i++) {
+        let cell = document.createElement("div");
+        if (i < 10) {
+            cell.classList.add('firstRow')
+        }
+        if (i % 10 === 0) {
+            cell.classList.add('firstColumn')
+        }
+
+        cell.classList.add("cellNull");
+        cell.classList.add("player");
+        cell.setAttribute("id", `${i}`);
+        playerDiv.appendChild(cell);
+    }
+
+
     // Render the board of the computer
     for (let i = 0; i < 100; i++) {
-          let cell = document.createElement("div");
-          if (i < 10) {
+        let cell = document.createElement("div");
+        if (i < 10) {
             cell.classList.add('firstRow')
-          }
-          if (i % 10 === 0) {
+        }
+        if (i % 10 === 0) {
             cell.classList.add('firstColumn')
-          }
+        }
 
-          cell.classList.add("cellNull");
-          cell.classList.add("comp");         
-          cell.classList.add(`${boardComp.cells[i]}`)
-          cell.setAttribute("id", `${i}`);
-          compDiv.appendChild(cell);
-      }
-    
+        cell.classList.add("cellNull");
+        cell.classList.add("comp");
+        cell.classList.add(`${boardComp.cells[i]}`)
+        cell.setAttribute("id", `${i}`);
+        compDiv.appendChild(cell);
+    }
+
 }
