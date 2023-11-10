18955bdc72d1efa524ecd5f360ac0b8f78ae0469
BalazsBanfi <banfibalazs81@gmail.com>
2023-11-10 13:47:57 +0100

    Set array 2d

--- src/render.js
+++ src/render.js
@@ -13,38 +13,42 @@ export const renderPage = () => {
     const compDiv = document.getElementById('boardComputer');
 
     // Render the board of the player with random ships
-    for (let i = 0; i < 100; i++) {
-        let cell = document.createElement("div");
-        if (i < 10) {
-            cell.classList.add('firstRow')
-        }
-        if (i % 10 === 0) {
-            cell.classList.add('firstColumn')
-        }
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let cell = document.createElement("div");
+            if (i === 0) {
+                cell.classList.add('firstRow')
+            }
+            if (j === 0) {
+                cell.classList.add('firstColumn')
+            }
 
-        cell.classList.add("cellNull");
-        cell.classList.add("player");
-        cell.classList.add(`${boardPlayer.cells[i]}`)
-        cell.setAttribute("id", `${i}`);
-        playerDiv.appendChild(cell);
+            cell.classList.add("cellNull");
+            cell.classList.add("player");
+            cell.classList.add(`${boardPlayer.cells[i][j]}`)
+            cell.setAttribute("id", `${i}`);
+            playerDiv.appendChild(cell);
+        }
     }
 
 
     // Render the board of the computer
-    for (let i = 0; i < 100; i++) {
-        let cell = document.createElement("div");
-        if (i < 10) {
-            cell.classList.add('firstRow')
-        }
-        if (i % 10 === 0) {
-            cell.classList.add('firstColumn')
-        }
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let cell = document.createElement("div");
+            if (i === 0) {
+                cell.classList.add('firstRow')
+            }
+            if (j === 0) {
+                cell.classList.add('firstColumn')
+            }
 
-        cell.classList.add("cellNull");
-        cell.classList.add("comp");
-        cell.classList.add(`${boardComp.cells[i]}`)
-        cell.setAttribute("id", `${i}`);
-        compDiv.appendChild(cell);
+            cell.classList.add("cellNull");
+            cell.classList.add("comp");
+            cell.classList.add(`${boardComp.cells[i][j]}`)
+            cell.setAttribute("id", `${i}`);
+            compDiv.appendChild(cell);
+        }
     }
 
     // Add event listener to the computer board cells
@@ -57,6 +61,4 @@ export const renderPage = () => {
         })
 
     })
-
-
 }
