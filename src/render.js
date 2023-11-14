381545e7a71614c8cf4ed852fe3dd4ab524994de
BalazsBanfi <banfibalazs81@gmail.com>
2023-11-14 21:24:42 +0100

    Add cross

--- src/render.js
+++ src/render.js
@@ -74,28 +74,86 @@ export const renderPage = () => {
 
     }
     let firstShoots = [33, 35, 44, 46, 53, 55, 64, 66];
+
+    // Return cross around the hitted shot without the missed cells
+    let crossShoot = (id) => {
+        let arr = []
+
+        let directions = [-1, 1, -10, 10];
+        let temp = 0;
+
+        // horizontal section of the cross
+        for (let i = 0; i < 2; i++) {
+            let tempArr = [];
+            for (let j = 1; j < 5; j++) {
+                temp = +id + (directions[i] * j);
+
+                if (Math.floor(id / 10) !== Math.floor(temp / 10)
+                    || document.getElementById(`p${temp}`).classList.contains('miss')) {
+                    break;
+                } else {
+                    tempArr.push(temp);
+                }
+            }
+            arr.push(tempArr);
+        }
+
+        // vertical section of the cross
+        for (let i = 2; i < 4; i++) {
+            let tempArr = [];
+            for (let j = 1; j < 5; j++) {
+                temp = +id + (directions[i] * j);
+
+                if (temp < 0 || temp > 99 || document.getElementById(`p${('0' + temp).slice(-2)}`).classList.contains('miss')) {
+                    break;
+                } else {
+                    tempArr.push(temp);
+                }
+
+
+            }
+            arr.push(tempArr);
+        }
+        console.table(arr)
+        return arr;
+
+    }
     let lastHit = false;
     const compMove = () => {
+        let targetedShoots = [];
         let setHitted = (diagonal) => {
+            // Removes from the possibilities the diagonals of the hitted cell
             let removes = [diagonal - 11, diagonal - 9, +diagonal + 9, +diagonal + 11];
             targetArr = [...targetArr.filter(x => !removes.includes(x))];
             firstShoots = [...firstShoots.filter(x => !removes.includes(x))];
+
+            // At the first shot of the ship initialize the cross targets
+            if (targetedShoots.length === 0) {
+                targetedShoots = crossShoot(diagonal);
+            }
         }
+
+        // After a hit calls the setHitted func
         if (lastHit) {
             setHitted(lastHit);
 
         } else {
+
+            // If the last shot missed, first tries the center of the board, later randomly the paired cells
             let attack = firstShoots.length > 0
                 ? firstShoots[Math.floor(Math.random() * firstShoots.length)]
                 : targetArr[Math.floor(Math.random() * targetArr.length)];
 
+            // Attacks the cell
             let eTarget = document.getElementById(`p${attack}`)
             let cellContent = playersBoard.receiveAttack(eTarget.id.slice(1));
 
+            // Delete the targeted cell from the possibilities from arrays
             targetArr = [...targetArr.filter(x => x != eTarget.id.slice(1))];
             firstShoots = [...firstShoots.filter(x => x != eTarget.id.slice(1))];
             let sunk = ''
 
+            // Check if hitted or missed the ship
             if (cellContent === 'didNotHit') {
                 eTarget.classList.add('miss');
                 playerInfoBox.innerHTML = `Mis! ${playersBoard.stillAlive} enemies ships remaining`
@@ -110,6 +168,4 @@ export const renderPage = () => {
             }
         }
     }
-
-
 }
