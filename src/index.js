35f664fd97a37f74f09e31df90adb207fd66e6c9
BalazsBanfi <banfibalazs81@gmail.com>
2023-12-05 19:27:10 +0100

    Add form style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { player } from "./factories/player.js";
 import { renderPage } from "./render.js";
 
-const playerOne = player("Balázs");
-const computer = player("computer");
 
+const computer = player("computer");
 const newGame = document.getElementById("newGame");
 newGame.addEventListener("submit", (e) => {
   e.preventDefault();
+  const playerOne = player(document.getElementById('name').value);
+  const myBoard = document.getElementById('myBoard');
+  myBoard.innerHTML = `${playerOne.name}'s board`;
   const controller = document.getElementById("controllers");
   controller.style.display = "none";
   renderPage();
