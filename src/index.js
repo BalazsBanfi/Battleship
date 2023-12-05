851b66d2adad0a91ad7ba1f0187ab6b7bc10f1b9
bally4h <banfibalazs81@gmail.com>
2023-12-05 16:34:07 +0100

    Start game panel and submit button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import { player } from './factories/player.js';
+import { player } from "./factories/player.js";
 import { renderPage } from "./render.js";
 
-const playerOne = player('Balázs');
-const computer = player('computer');
+const playerOne = player("Balázs");
+const computer = player("computer");
 
-renderPage();
+const newGame = document.getElementById("newGame");
+newGame.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const controller = document.getElementById("controllers");
+  controller.style.display = "none";
+  renderPage();
+});
