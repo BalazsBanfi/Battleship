973d057349cb92c2b3cea8c3e6a182145017c036
bally4h <banfibalazs81@gmail.com>
2023-12-06 08:51:04 +0100

    Set finish game function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { player } from "./factories/player.js";
 import { renderPage } from "./render.js";
 
-
 const computer = player("computer");
 const newGame = document.getElementById("newGame");
 newGame.addEventListener("submit", (e) => {
   e.preventDefault();
-  const playerOne = player(document.getElementById('name').value);
-  const myBoard = document.getElementById('myBoard');
+  const playerOne = player(document.getElementById("name").value);
+  const myBoard = document.getElementById("myBoard");
   myBoard.innerHTML = `${playerOne.name}'s board`;
   const controller = document.getElementById("controllers");
   controller.style.display = "none";
