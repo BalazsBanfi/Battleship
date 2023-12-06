56c811aa05164c07d03e9859fa40d951db99c7bf
bally4h <banfibalazs81@gmail.com>
2023-12-06 10:25:30 +0100

    Add html webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { player } from "./factories/player.js";
 import { renderPage } from "./render.js";
+import "./style.css";
 
 const computer = player("computer");
 const newGame = document.getElementById("newGame");
